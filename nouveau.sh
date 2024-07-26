df9ed667acf9a867ae260c6828f64dd36f37f15d
KilianCaillot <80099227+KilianCaillot@users.noreply.github.com>
2024-07-26 15:41:04 +0400

    Update nouveau.sh

--- nouveau.sh
+++ nouveau.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Chemin vers le fichier Python
-PYTHON_SCRIPT="/local/repository/creation.py"
+PYTHON_SCRIPT="/local/repository/calcul.py"
 
 # Exécuter le script Python
 python3 "$PYTHON_SCRIPT"
